7f51746a24fbfb2652d7cd08d835f671162ca26f
Junya Sugiyama <16234046@edu.cc.saga-u.ac.jp>
2018-07-29 01:34:43 +0900

    edの追加

---
+++ works/StoryAndEnding/島義勇/html/ed/js/story_shima_qestion.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.serectbox1_bun').click(function(){
+       alert('残念！！不正解！！ もう一度考えてみよう！！')
+    });
+});
+
+$(function(){
+    $('.serectbox3_bun').click(function(){
+       alert('残念！！不正解！！ もう一度考えてみよう！！')
+    });
+});
